a48f490b94551a1acde7704f631fd14ef161c8c2
Kaiyang <kkykhaw03.23@ichat.sp.edu.sg>
2024-06-07 17:52:39 +0800

    calculate bmi in lab2.py file

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,15 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")   
+print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def calculate_bmi(height, weight):     
+    print("Height = " + str(height)) 
+    print("Weight = " + str(weight)) 
+    #Add code here to calculate BMI     
+    bmi = weight/height**2
+    #Add code here to display calculate BMI     
+    print(f"bmi of the peron is :\t{bmi}") 
+    
+
+calculate_bmi(weight=57, height=1.73) 
+
+ 
+
